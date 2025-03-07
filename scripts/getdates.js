6980404c1a22ea58b3aae3c3a170769fe615c93d
Uriyah Sam <uriyahadrielsam@gmail.com>
2025-03-07 05:13:31 -0700

    Week One Project - First Commit

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get the current year dynamically
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Get the last modified date
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
